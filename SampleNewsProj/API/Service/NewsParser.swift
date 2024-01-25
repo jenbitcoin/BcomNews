87809b4add0d13ab3a0eca6404b67e6b0a64591e
Jen <jen@bitcoin.com>
2024-01-25 10:27:13 +0800

    Add more details

--- SampleNewsProj/API/Service/NewsParser.swift
+++ SampleNewsProj/API/Service/NewsParser.swift
@@ -7,13 +7,49 @@
 
 import Foundation
 
+public struct AdjacentNews: Codable {
+    public struct News: Codable {
+        public let id: Int
+        public let name: String
+        public let slug: String
+    }
+    
+    public let prev: AdjacentNews.News
+    public let Next: AdjacentNews.News
+}
+
 public struct NewsJSON: Codable {
+    public let id: Int
+    public let date: String
     public let title: String
+    public let slug: String
     public let content: String
-    public let meta_title: String
-    public let meta_description: String
-    public let barker_title: String
-    public let late_barker_title: String
+    public let adjacent_posts: AdjacentNews
+}
+
+public struct NewsAuthor: Codable {
+    public let id: String
+    public let name: String
+    public let slug: String
+    public let info: String
+    public let avatar: String
+}
+
+public struct NewsCategory: Codable {
+    public let id: Int
+    public let name: String
+    public let slug: String
+}
+
+public struct NewsThumbnail: Codable {
+    public let thumbnail: String
+    public let nano: String
+    public let micro: String
+    public let small: String
+    public let medium: String
+    public let big: String
+    public let large: String
+    public let featured_image: String
 }
 
 public struct NewsListItem: Codable {
@@ -21,7 +57,10 @@ public struct NewsListItem: Codable {
     public let date: String
     public let title: String
     public let slug: String
+    public let author: NewsAuthor
     public let excerpt: String
+    public let categories: [NewsCategory]
+    public let thumbnail: NewsThumbnail
 }
 
 public struct NewsListJSON: Codable {

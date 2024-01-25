b341bfc509e8e339d783ff876e494445ad6834d2
Jen <jen@bitcoin.com>
2024-01-25 12:57:49 +0800

    Cleanup webview

--- SampleNewsProj/Screens/NewsDetailScreen.swift
+++ SampleNewsProj/Screens/NewsDetailScreen.swift
@@ -10,37 +10,47 @@ import SwiftUI
 
 struct WebScreen: View {
     var htmlString: String
+    @Binding var webViewHeight: CGFloat
+    
     var body: some View {
         WebViewRepresentable(url: nil,
-                             htmlString: htmlString)
+                             htmlString: htmlString,
+                             contentHeight: $webViewHeight)
     }
 }
 
 struct NewsDetailScreen<Model>: View where Model: NewsDetailViewModelProtocol {
     @StateObject var viewModel: Model
+    @State var webViewHeight: CGFloat = 0
 
     var body: some View {
-        Group {
+        ZStack {
             switch viewModel.state {
             case .loading:
                 ProgressView("Loading")
             case .screen:
                 if let news = viewModel.news {
-                    VStack {
-                        Text(news.title)
-                            .foregroundStyle(.black)
-                            .multilineTextAlignment(.leading)
-                            .font(.system(size: 20, weight: .bold))
-                        
-                        AsyncImage(url: URL(string: news.thumbnailURLString)) { image in
-                            image.image?
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                         }
-                        
-                        WebScreen(htmlString: news.content)
-                            .navigationTitle(news.title)
-                            .navigationBarTitleDisplayMode(.inline)
+                    GeometryReader { reader in
+                        ScrollView {
+                            VStack {
+                                Text(news.title)
+                                    .foregroundStyle(.black)
+                                    .multilineTextAlignment(.leading)
+                                    .font(.system(size: 20, weight: .bold))
+                                
+                                AsyncImage(url: URL(string: news.thumbnailURLString)) { image in
+                                    image.image?
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                 }
+                                
+                                WebScreen(htmlString: news.content, webViewHeight: $webViewHeight)
+                                    .frame(width: reader.size.width, height: max(reader.size.height, self.webViewHeight))
+                                    .edgesIgnoringSafeArea(.bottom)
+                                    .navigationTitle(news.title)
+                                    .navigationBarTitleDisplayMode(.inline)
+                            }
+                        }
                     }
                 }
             case .error:
--- SampleNewsProj/Screens/WebViewRepresentable.swift
+++ SampleNewsProj/Screens/WebViewRepresentable.swift
@@ -11,17 +11,48 @@ import WebKit
 struct WebViewRepresentable: UIViewRepresentable {
     let url: URL?
     let htmlString: String?
+    @Binding var contentHeight: CGFloat
 
-    func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
+    func makeUIView(context: UIViewRepresentableContext<WebViewRepresentable>) -> WKWebView {
+        let webView = WKWebView()
+        webView.scrollView.isScrollEnabled = false
+        webView.navigationDelegate = context.coordinator
+        return webView
     }
-    
-    func updateUIView(_ webView: WKWebView, context: Context) {
+
+    func updateUIView(_ wkWebView: WKWebView, context: UIViewRepresentableContext<WebViewRepresentable>) {
         if let html = self.htmlString {
-            webView.loadHTMLString(html, baseURL: nil)
+            wkWebView.loadHTMLString(html, baseURL: nil)
         } else if let url = self.url {
             let request = URLRequest(url: url)
-            webView.load(request)
+            wkWebView.load(request)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(contentHeight: $contentHeight)
+    }
+
+    class Coordinator: NSObject, WKNavigationDelegate {
+        @Binding var contentHeight: CGFloat
+        var resized = false
+
+        init(contentHeight: Binding<CGFloat>) {
+            self._contentHeight = contentHeight
+        }
+
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            webView.evaluateJavaScript("document.readyState") { complete, _ in
+                guard complete != nil else { return }
+                webView.evaluateJavaScript("document.body.scrollHeight") { height, _ in
+                    guard let height = height as? CGFloat else { return }
+
+                    if !self.resized {
+                        self.contentHeight = height
+                        self.resized = true
+                    }
+                }
+            }
         }
     }
 }

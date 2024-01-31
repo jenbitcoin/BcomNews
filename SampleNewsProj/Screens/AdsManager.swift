8a286a2b85173793535978f420f510e75645aa6a
Jen <jen@bitcoin.com>
2024-01-31 16:08:20 +0800

    Update AdsManager.swift

--- SampleNewsProj/Screens/AdsManager.swift
+++ SampleNewsProj/Screens/AdsManager.swift
@@ -9,11 +9,10 @@ import Foundation
 import GoogleMobileAds
 
 final class AdsManager: NSObject {
-    private var adLoader: GADAdLoader?
-    var rootViewController: UIViewController
+    private var bannerView: GADBannerView!
+    private var rootViewController: UIViewController
         
-    init(adLoader: GADAdLoader? = nil, rootViewController: UIViewController) {
-        self.adLoader = adLoader
+    init(rootViewController: UIViewController) {
         self.rootViewController = rootViewController
     }
 
@@ -22,30 +21,64 @@ final class AdsManager: NSObject {
             debugPrint("setupGoogleAds completion status: ", status)
         }
     }
-    
-    func loadAds() {
-        adLoader?.load(GADRequest())
+
+    func initializeAdBanner() {
+        bannerView = GADBannerView(adSize: GADAdSizeBanner)
+        bannerView.adUnitID = "ca-app-pub-4413360973090930/2566205549"
+        bannerView.rootViewController = self.rootViewController
+        bannerView.load(GADRequest())
     }
     
-    func initializeAdLoader() {
-        let multipleAdOptions = GADMultipleAdsAdLoaderOptions()
-        multipleAdOptions.numberOfAds = 5;
+    private func addBannerViewToView(_ bannerView: GADBannerView) {
+        guard let view = self.rootViewController.view else {
+            return
+        }
         
-        adLoader = GADAdLoader(adUnitID: "ca-app-pub-3940256099942544/3986624511",
-                               rootViewController: self.rootViewController,
-                               adTypes: [.native],
-                               options: [multipleAdOptions])
-        adLoader?.delegate = self
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(bannerView)
+        view.addConstraints(
+              [NSLayoutConstraint(item: bannerView,
+                                  attribute: .bottom,
+                                  relatedBy: .equal,
+                                  toItem: view.safeAreaLayoutGuide,
+                                  attribute: .bottom,
+                                  multiplier: 1,
+                                  constant: 0),
+               NSLayoutConstraint(item: bannerView,
+                                  attribute: .centerX,
+                                  relatedBy: .equal,
+                                  toItem: view,
+                                  attribute: .centerX,
+                                  multiplier: 1,
+                                  constant: 0)
+              ])
     }
 }
 
-extension AdsManager: GADAdLoaderDelegate {
-    func adLoader(_ adLoader: GADAdLoader, didFailToReceiveAdWithError error: Error) {
-        debugPrint("Did fail to receive ad")
+extension AdsManager: GADBannerViewDelegate {
+    func bannerViewDidReceiveAd(_ bannerView: GADBannerView) {
+      print("bannerViewDidReceiveAd")
+        addBannerViewToView(bannerView)
     }
-    
-    public func adLoader(_ adLoader: GADAdLoader,
-                         didReceive nativeAd: GADNativeAd) {
-        debugPrint("did receive ad successfully")
+
+    func bannerView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: Error) {
+      print("bannerView:didFailToReceiveAdWithError: \(error.localizedDescription)")
+    }
+
+    func bannerViewDidRecordImpression(_ bannerView: GADBannerView) {
+      print("bannerViewDidRecordImpression")
+    }
+
+    func bannerViewWillPresentScreen(_ bannerView: GADBannerView) {
+      print("bannerViewWillPresentScreen")
+    }
+
+    func bannerViewWillDismissScreen(_ bannerView: GADBannerView) {
+      print("bannerViewWillDIsmissScreen")
+    }
+
+    func bannerViewDidDismissScreen(_ bannerView: GADBannerView) {
+      print("bannerViewDidDismissScreen")
     }
 }
